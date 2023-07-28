27a7e554d2b3e4a3cd172ccdf72d4c063b1a5e53
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-07-28 12:00:16 -0300

    feat: portfolio section

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,7 +1,41 @@
-import styles from './Portfolio.module.css'
+import './Portfolio.css'
+import IMG1 from '../../assets/authentication.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Authentication API',
+    github: 'https://github.com/rafaelxvr/authentication-api',
+    demo: 'https://auth-clean-api-fdbc0cccb71c.herokuapp.com'
+  }
+]
 
 export function Portfolio() {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio_container'>
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className='portfolio_item'>
+              <div className='portfolio_item_image'>
+                <img src={image} alt={title} />
+              </div>
+      
+                <h3>{title}</h3>
+                <div className="portfolio_item_cta">
+                <a href={github} className='btn' target='_blank'>Github</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
