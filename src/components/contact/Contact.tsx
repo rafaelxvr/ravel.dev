f004d6dd315dfb4dcfd50db4d45925b091f032c0
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-07-28 15:35:31 -0300

    feat: footer and contact sections

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,7 +1,57 @@
-import styles from './Contact.module.css'
+import './Contact.css'
+import React from "react";
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiInstagramLine } from 'react-icons/ri' 
+import { BsTelegram } from 'react-icons/bs'
+import emailjs from 'emailjs-com'
 
 export const Contact = () => {
+
+  function sendEmail(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    emailjs.sendForm("service_u783p0h", "template_faba91j",  e.currentTarget, "019ZSgKm45fBFjN8H")
+    
+    e.currentTarget.reset()
+  }
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+        <div className="contact_options">
+
+          <article className="contact_option">
+            <MdOutlineEmail className='contact_option_icon' />
+            <h4>Email</h4>
+            <h5>rafaellucasxc@gmail.com</h5>
+            <a href='mailto:rafaellucasxc@gmail.com' target='_blank'>Send a Message</a>
+          </article>
+
+          <article className="contact_option">
+            <RiInstagramLine className='contact_option_icon' />
+            <h4>Instagram</h4>
+            <h5>@rafaelxv_</h5>
+            <a href='ig.me/m/rafaelxv_' target='_blank'>Send a Message</a>
+          </article>
+
+          <article className="contact_option">
+            <BsTelegram className='contact_option_icon' />
+            <h4>Telegram</h4>
+            <h5>@rafaelxv</h5>
+            <a href='https://t.me/rafaelxv' target='_blank'>Send a Message</a>
+          </article>
+        </div>
+
+        <form onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type='email' name='email' placeholder='Your Email' required/>
+          <textarea name='message' placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,36 @@
-import styles from './Footer.module.css'
+import './Footer.css'
+import { FaFacebookF } from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
+import { IoLogoTwitter } from 'react-icons/io'
+import logo from '../../assets/logo.svg'
 
 export function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer_logo'>
+        <img src={logo} alt='logo' />
+        UNRAVEL
+      </a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#solutions'>Solutions</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#testimonials'>Testimonials</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer_socials'>
+        <a href='https://www.facebook.com'><FaFacebookF /></a>
+        <a href='https://www.instagram.com'><FiInstagram /></a>
+        <a href='https://www.twitter.com'><IoLogoTwitter /></a>
+      </div>
+
+      <div className='footer_copyright'>
+        <small>&copy; Unravel Tech. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
